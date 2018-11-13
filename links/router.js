0e1bd0030a953a4aa1bb8c19fd4b57a9beb0fe83
Jason Vance <=>
2018-11-12 19:08:01 -0600

    add links router

---
+++ links/router.js
@@ -0,0 +1,18 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//const {Link} = require('./models');
+
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+// Post to register a new user
+router.get('/', (req, res) => {
+    return res.status(200).json({
+        message: "LINKS GO HERE"
+    });
+})
+
+module.exports = {router};
