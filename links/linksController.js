8bc635e83a9242753afe76e75e91419cc0fb4016
Jason Vance <=>
2018-11-15 17:21:03 -0600

    implement delete link api

--- links/linksController.js
+++ links/linksController.js
@@ -1,6 +1,12 @@
 const urlRegex = require('url-regex');
-const { getTitle, getLogo } = require('../utils');
-const { Link, Category } = require('./models');
+const {
+    getTitle,
+    getLogo
+} = require('../utils');
+const {
+    Link,
+    Category
+} = require('./models');
 
 
 
@@ -12,28 +18,67 @@ const { Link, Category } = require('./models');
 //    console.log("TEST TITLE", testTitle);
 //   })();
 
-exports.getLinks = async function(req, res) {
-    Link.find({ userId: req.user.id })
-    .then(links => {
-        return res.status(200).json({
-            data: links
+
+//Gets links for a particular user - userId is located via the jwt payload
+exports.getLinks = function (req, res) {
+    Link.find({
+            userId: req.user.id
+        })
+        .then(links => {
+            return res.status(200).json({
+                data: links
+            });
+        })
+        .catch(err => {
+            const message = `Unable to retrieve links for user ${req.user.id}`
+            console.error(message);
+            return res.status(500).json({
+                error: {
+                    message
+                }
+            })
         });
-    });
-    
 }
 
-exports.createLink = async function( req, res ) {    
+exports.deleteLink = function (req, res) {
+    const id = req.params.id;
+    console.log("ID TO DELETE", id);
+    Link.findByIdAndRemove(id)    
+        .then((link) => {
+            if (!link) {
+                return res.status(400).json({
+                    message: `Unable to delete. No link with id ${id} found.`
+                })
+            }
+            return res.status(200).json({
+                message: 'Record Deleted'
+            });
+        })
+        .catch(err => {
+            const message = `Unable to delete link with id of ${id}`
+            console.error(err);
+            return res.status(500).json({
+                error: {
+                    message
+                }
+            })
+        })
+}
+
+exports.createLink = async function (req, res) {
     const split = req.path.split('--');
     let list = '';
     let path = '';
     if (split.length > 1) {
-      list = split[0].replace(/^\/+/g, '');
-      path = split[1];
+        list = split[0].replace(/^\/+/g, '');
+        path = split[1];
     } else {
-      path = split[0].replace(/^\/+/g, '');
+        path = split[0].replace(/^\/+/g, '');
     }
     // If path is not a valid url, send error response 
-    if (!urlRegex({exact: true}).test(path)) {
+    if (!urlRegex({
+            exact: true
+        }).test(path)) {
         return res.status(400).json({
             error: {
                 message: `path ${path} is not a valid url`
@@ -41,7 +86,7 @@ exports.createLink = async function( req, res ) {
         })
     }
     let query = '';
-    if (req.query && Object.keys(req.query).length) {        
+    if (req.query && Object.keys(req.query).length) {
         query = '?' + Object.keys(req.query).map(key => key + '=' + req.query[key]).join('&');
         path += query;
     }
@@ -52,24 +97,46 @@ exports.createLink = async function( req, res ) {
     try {
         title = await getTitle(path);
         favIcon = await (getLogo(path));
-        if (list) {        
-            category = await Category.findOne({name: list.toLowerCase()});           
+        if (list) {
+            category = await Category.findOne({
+                name: list.toLowerCase()
+            });
             if (!category) {
-                category = await Category.create({ name: list.toLocaleLowerCase() })           
-            }           
+                category = await Category.create({
+                    name: list.toLocaleLowerCase()
+                })
+            }
 
-            link = await Link.create({ href: path, category: category._id, userId: req.user.id, title, favIcon });
+            link = await Link.create({
+                href: path,
+                category: category._id,
+                userId: req.user.id,
+                title,
+                favIcon
+            });
             if (link) {
-                return res.status(201).json({ link })
-            }  else {
-                return res.status(500).json({ error: 'Unable to create link'});
-            }                                    
-        } else {           
-            link = await Link.create({ href: path, title, userId: req.user.id, favIcon });          
-            return res.status(201).json({ link });
+                return res.status(201).json({
+                    link
+                })
+            } else {
+                return res.status(500).json({
+                    error: 'Unable to create link'
+                });
+            }
+        } else {
+            link = await Link.create({
+                href: path,
+                title,
+                userId: req.user.id,
+                favIcon
+            });
+            return res.status(201).json({
+                link
+            });
         }
-    } catch(err) {
-        return res.status(500).json({ error: err.message })
+    } catch (err) {
+        return res.status(500).json({
+            error: err.message
+        })
     }
-  };
-  
+};
--- links/router.js
+++ links/router.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const passport = require('passport');
 const jwtAuth = passport.authenticate('jwt', { session: false });
-const { getLinks } = require('./linksController');
+const { getLinks, deleteLink } = require('./linksController');
 
 //const {Link} = require('./models');
 
 const router = express.Router();
 
 router.get('/', jwtAuth, getLinks);
+
+router.delete('/:id', jwtAuth, deleteLink);
 
 module.exports = {router};
