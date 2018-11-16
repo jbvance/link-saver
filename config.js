1cfbdfdc6ac88c5ec096000143aaec0cdfb25a37
Jason Vance <=>
2018-11-15 22:52:40 -0600

    update comments

--- config.js
+++ config.js
@@ -14,9 +14,3 @@ exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 
-// const env = process.env.NODE_ENV || 'development';
-// if (env === 'development' || env === 'test') {
-//     const config = require('./config.json');
-//     const envConfig = config[env];
-//     console.log(envConfig);
-// }
